b9d14f75a0533aa74ba1d76be8ed89116d4886c5
Patrick Schriner <p.schriner@gmail.com>
2016-02-29 19:06:41 +0100

    [BUGFIX] TER wooes

--- ext_emconf.php
+++ ext_emconf.php
@@ -17,7 +17,7 @@ $EM_CONF[$_EXTKEY] = array (
     'constraints' => array (
         'depends' => array(
             'php' => '5.5.0-0.0.0',
-            'typo3' => '6.2.7-8.0.99',
+            'typo3' => '6.2.7-7.6.99',
         ),
         'conflicts' => array(),
         'suggests' => array(),
