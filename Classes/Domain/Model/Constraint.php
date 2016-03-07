098ed87cc201b3764c9cd23e01dbaa3fee366a20
Patrick Schriner <patrick.schriner@diemedialen.de>
2016-03-07 10:48:25 +0100

    [BUGFIX] PHPDoc + CGL
    Change-Id: Ie0d06152719d93f9bfe3a6d52d6eb6c4a7710560

--- Classes/Domain/Model/Constraint.php
+++ Classes/Domain/Model/Constraint.php
@@ -15,7 +15,8 @@ namespace DieMedialen\DmDeveloperlog\Domain\Model;
  */
 
 /**
- * Constraints for log entries
+ * Constraints for log entry searches
+ * This object should never be persisted
  */
 class Constraint extends \TYPO3\CMS\Extbase\DomainObject\AbstractEntity
 {
@@ -23,42 +24,90 @@ class Constraint extends \TYPO3\CMS\Extbase\DomainObject\AbstractEntity
      * @var int
      */
     protected $severity = -1;
-    
-    
+
+
     /**
      * @var string
      */
     protected $search = '';
-    
+
     /**
      * @var string
      */
     protected $extkey = '';
-    
+
+    /**
+     * Default constructor
+     */
+    public function __construct()
+    {
+    }
+
+    /**
+     * added to prevent the deprecation message
+     * in Extbase\DomainObject\AbstractDomainObject
+     *
+     * @todo the constraints model needs another way of storing
+     * persisted search data than serialisation
+     */
+    public function __wakeup()
+    {
+    }
+
+    /**
+     * Set the severity
+     *
+     * @param string $severity
+     */
     public function setSeverity($severity)
     {
         $this->severity = (int)$severity;
     }
-    
+
+    /**
+     * Get the severity
+     *
+     * @return string
+     */
     public function getSeverity(){
         return $this->severity;
     }
-    
+
+    /**
+     * Set the search word
+     *
+     * @param string $search
+     */
     public function setSearch($search)
     {
         $this->search = $search;
     }
-    
+
+    /**
+     * Get the search word
+     *
+     * @return string
+     */
     public function getSearch()
     {
         return $this->search;
     }
-   
-    public function setExtkey($extkey) 
+
+    /**
+     * Set the extension key
+     *
+     * @param string $extkey
+     */
+    public function setExtkey($extkey)
     {
         $this->extkey = $extkey;
     }
-    
+
+    /**
+     * Get the extension key
+     *
+     * @return string
+     */
     public function getExtkey()
     {
         return $this->extkey;
--- Classes/Domain/Model/Logentry.php
+++ Classes/Domain/Model/Logentry.php
@@ -23,143 +23,186 @@ class Logentry extends \TYPO3\CMS\Extbase\DomainObject\AbstractEntity
      * @var \DateTime
      */
     protected $crdate;
-    
+
     /**
      * @var string
      */
     protected $requestId;
-    
+
     /**
      * @var int
      */
     protected $requestType;
-    
+
     /**
      * @var int
      */
     protected $beUser;
-    
+
     /**
      * @var int
      */
     protected $feUser;
-    
+
     /**
      * @var int
      */
     protected $line;
-    
+
     /**
      * @var string
      */
     protected $location;
-    
+
     /**
      * @var string
      */
     protected $extkey;
-    
+
     /**
      * @var string
      */
     protected $message;
-    
+
     /**
      * @var int
      */
     protected $severity;
-    
+
     /**
      * @var string
      */
     protected $dataVar;
-    
+
     /**
      * @var int
      */
     protected $workspaceUid;
-    
+
     /**
-     * @var boolean 
+     * @var boolean
      */
     protected $system;
-    
-    public function getCrdate() {
+
+    /**
+     * Get the creation date
+     *
+     * @return DateTime
+     */
+    public function getCrdate()
+    {
         return $this->crdate;
     }
-    
+
     /**
+     * Get the request ID
+     *
      * @return string
      */
-    public function getRequestId() {
+    public function getRequestId()
+    {
         return $this->requestId;
     }
-    
+
     /**
+     * Get the request type. This is a bitmasked value.
+     * The bitmask is defined in \TYPO3\CMS\Core\Bootstrap
+     *
      * @return int
      */
-    public function getRequestType() {
+    public function getRequestType()
+    {
         return $this->requestType;
     }
-    
+
     /**
+     * Get the backend user UID.
+     * Even on a frontend request this can be defined.
+     * It's an int for performance reasons.
+     *
      * @return int
      */
-    public function getBeUser() {
+    public function getBeUser()
+    {
         return $this->beUser;
     }
-    
+
     /**
+     * Get the frontend user UID.
+     * It's an int for performance reasons.
+     *
      * @return int
      */
-    public function getFeUser() {
+    public function getFeUser()
+    {
         return $this->feUser;
     }
-    
+
     /**
+     * Get the line on which the devLog method was called
+     *
      * @return int
      */
-    public function getLine() {
+    public function getLine()
+    {
         return $this->line;
     }
-    
+
     /**
+     * Get the file in which the devLog method was called
+     *
      * @return string
      */
-    public function getLocation() {
+    public function getLocation()
+    {
         return $this->location;
     }
-    
+
     /**
+     * Get the extension key
+     * Even though it is defined as "Extension Key" this is not enforced
+     *
      * @return string
      */
-    public function getExtkey() {
+    public function getExtkey()
+    {
         return $this->extkey;
     }
-    
+
     /**
+     * Get the message passed to "devLog"
+     *
      * @return string
      */
-    public function getMessage() {
+    public function getMessage()
+    {
         return $this->message;
     }
-    
+
     /**
+     * Get the severity of the "devLog" call
+     *
      * @return int
      */
-    public function getSeverity() {
+    public function getSeverity()
+    {
         return $this->severity;
     }
-    
+
     /**
+     * Get the additional data provided for the "devLog" call
+     * This is a json_encoded something. As it is stored partially if encoding fails
+     * there is no guarantee that it can be decoded
+     *
      * @return string
      */
-    public function getDataVar() {
+    public function getDataVar()
+    {
         return $this->dataVar;
     }
 
     /**
-     * Get workspace
+     * Get workspace UID
      *
      * @return int
      */
@@ -167,8 +210,15 @@ class Logentry extends \TYPO3\CMS\Extbase\DomainObject\AbstractEntity
     {
         return (int)$this->workspaceUid;
     }
-    
-    public function isSystemLogEntry() {
+
+    /**
+     * Check whether this extension thinks the devLog call came from a
+     * system extension (naive check via file path).
+     *
+     * @return boolean
+     */
+    public function isSystemLogEntry()
+    {
         return $this->system;
     }
 }
