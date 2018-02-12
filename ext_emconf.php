374104fe8d1926c9ec16a95aac12da4ec31b0a42
Patrick Schriner <patrick.schriner@diemedialen.de>
2018-02-12 15:24:48 +0100

    Raise supported TYPO3 version, update license

--- ext_emconf.php
+++ ext_emconf.php
@@ -17,7 +17,7 @@ $EM_CONF[$_EXTKEY] = array (
     'constraints' => array (
         'depends' => array(
             'php' => '5.5.0-0.0.0',
-            'typo3' => '6.2.7-8.7.10',
+            'typo3' => '6.2.7-9.1.0',
         ),
         'conflicts' => array(),
         'suggests' => array(),
