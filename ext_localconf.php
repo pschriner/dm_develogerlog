b2b88435cac34edc699390ea210385fc1456d553
Patrick Schriner <patrick.schriner@diemedialen.de>
2019-02-13 18:04:42 +0100

    Try to get around compatibility restrictions

--- ext_localconf.php
+++ ext_localconf.php
@@ -12,3 +12,15 @@ if (!is_array($GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['scheduler']['tasks']['T
         'expirePeriod' => '30'
     ];
 }
+
+if (!class_exists(\TYPO3Fluid\Fluid\Core\ViewHelper\AbstractViewHelper::class)) {
+    $GLOBALS['TYPO3_CONF_VARS']['SYS']['Objects']['DieMedialen\\DmDeveloperlog\\ViewHelpers\\TYPO3Fluid\\UsernameViewHelper'] = [
+        'className' => 'DieMedialen\\DmDeveloperlog\\ViewHelpers\\UsernameViewHelper'
+    ];
+    $GLOBALS['TYPO3_CONF_VARS']['SYS']['Objects']['DieMedialen\\DmDeveloperlog\\ViewHelpers\\TYPO3Fluid\\BitMaskViewHelper'] = [
+        'className' => 'DieMedialen\\DmDeveloperlog\\ViewHelpers\\BitMaskViewHelper'
+    ];
+    $GLOBALS['TYPO3_CONF_VARS']['SYS']['Objects']['DieMedialen\\DmDeveloperlog\\ViewHelpers\\TYPO3Fluid\\MapToHelperClassViewHelper'] = [
+        'className' => 'DieMedialen\\DmDeveloperlog\\ViewHelpers\\MapToHelperClassViewHelper'
+    ];
+}
