d5d82575d8df8d86b0e2c2e7983eb28cf5152e45
Patrick Schriner <patrick.schriner@diemedialen.de>
2019-02-13 17:20:16 +0100

    [BUGFIX] Raise maximum version

--- ext_emconf.php
+++ ext_emconf.php
@@ -17,7 +17,7 @@ $EM_CONF[$_EXTKEY] = array (
     'constraints' => array (
         'depends' => array(
             'php' => '5.5.0-0.0.0',
-            'typo3' => '6.2.7-9.5.0',
+            'typo3' => '6.2.7-9.5.99',
         ),
         'conflicts' => array(),
         'suggests' => array(),
